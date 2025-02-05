13d5362738d1ed9f2efb0e338184edc58887613e
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-02-05 23:27:53 +0700

    updated

--- data_structure/graph/graph.cpp
+++ data_structure/graph/graph.cpp
@@ -5,5 +5,5 @@ using namespace std;
 
 int main()
 {
-    vector<vector<int>> edges = [ [ 0, 1 ], [ 0, 2 ], [ 3, 5 ], [ 5, 4 ], [ 4, 3 ] ];
+    vector<vector<int>> edges = [[0, 1 ], [ 0, 2 ], [ 3, 5 ], [ 5, 4 ], [ 4, 3 ] ];
 }
